48c9c4c5bc36b79203ba08c9557bde8d3dbe6eef
J-Guenther <jannik.guentheg@gmail.com>
2021-06-26 16:18:34 +0200

    added compiled files

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export { sayHello } from './math/vector2';
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+var vector2_1 = require("./math/vector2");
+Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return vector2_1.sayHello; } });
